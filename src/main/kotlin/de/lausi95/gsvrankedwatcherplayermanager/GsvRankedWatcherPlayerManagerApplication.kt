02817fb443abc4c5bc288820323b3cc7e0de2a09
Tom Lausmann <tomlausmann@gmail.com>
2023-12-19 22:22:03 +0100

    Initial Commit

---
+++ src/main/kotlin/de/lausi95/gsvrankedwatcherplayermanager/GsvRankedWatcherPlayerManagerApplication.kt
@@ -0,0 +1,11 @@
+package de.lausi95.gsvrankedwatcherplayermanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GsvRankedWatcherPlayerManagerApplication
+
+fun main(args: Array<String>) {
+  runApplication<GsvRankedWatcherPlayerManagerApplication>(*args)
+}
