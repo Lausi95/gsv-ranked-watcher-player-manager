0c3fcda3f7678b876884a9e43f2ce8b6112cba07
Tom Lausmann <tomlausmann@gmail.com>
2023-12-20 07:19:15 +0100

    Add Spring Boot MongoDB Starter to build

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-reflect")
 
   implementation("org.springframework.boot:spring-boot-starter")
+  implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
   implementation("org.springframework.kafka:spring-kafka")
 
   implementation("io.github.miniclem:kotlin-api-riot-wrapper:0.3")
