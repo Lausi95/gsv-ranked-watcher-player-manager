3d81f83cf157ccdf76d77bcc9d226a1f101765bd
Tom Lausmann <tomlausmann@gmail.com>
2023-12-20 07:29:42 +0100

    Remove unnecessary and sole documentation

--- src/main/kotlin/de/lausi95/gsvrankedwatcherplayermanager/domain/model/PlayerResolver.kt
+++ src/main/kotlin/de/lausi95/gsvrankedwatcherplayermanager/domain/model/PlayerResolver.kt
@@ -2,11 +2,5 @@ package de.lausi95.gsvrankedwatcherplayermanager.domain.model
 
 interface PlayerResolver {
 
-  /**
-   * Resolves a player based on the given summoner name.
-   *
-   * @param summonerName the name of the summoner to resolve
-   * @return the resolved Player object, or null if no Player is found
-   */
   fun resolvePlayer(summonerName: String): Player?
 }
